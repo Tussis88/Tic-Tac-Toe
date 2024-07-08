31597aaaa378e8d3674b37d33d7a22a55312139f
Andrea2 <andreab88@hotmail.it>
2024-07-08 18:01:33 +0200

    inizato wincheck

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ const gameBoard = (function () {
     const getBoard = () => board;
 
     const printBoard = () => {
-        const printableBoard = board.map(row => row.map(cell => cell.getValue().getToken ? cell.getValue().getToken() : " "));
+        const printableBoard = board.map(row => row.map(cell => cell.getValue() === 0 ? " " : cell.getValue().getToken()));
         console.table(printableBoard)
     };
 
@@ -42,15 +42,41 @@ const gameControl = (function () {
         console.log(`${getCurrentPlayer().getName()}'s turn.`)
         gameBoard.addToken(cell, getCurrentPlayer());
         gameBoard.printBoard();
+        if (winCheck()) {
+            console.log(`${getCurrentPlayer().getName()} is the winner`);
+        }
         switchTurn();
         console.log(`next turn: ${getCurrentPlayer().getName()}`)
     };
+    //TODO
+    const winCheck = () => {
+        const board = gameBoard.getBoard();
+        for (let i = 0; i < 3; i++) {
+            if (board[i].every(cell => cell.getValue().getToken)) {
+                const winner = board[i].every(cell => cell.getValue().getToken() === board[i][0].getValue().getToken());
+                return winner;
+            }
+            const firstCol = board[0][i].getValue().getToken ? board[0][i] : null;
+            for (let j = 1; j < 3; j++) {
+                if (!firstCol || firstCol.getValue().getToken() !== board[j][i].getValue().getToken()) {
+                    return false;
+                }
+            }
+        }
+        // const rowWin = board.map(row => {
+        //     if (row.every(cell => cell.getValue().getToken)) {
+        //         return row.every(cell => cell.getValue().getToken() === row[0].getValue().getToken());
+        //     }
+        // })
+
+        // return rowWin;
+    }
 
     return { getCurrentPlayer, switchTurn, playRound };
 })();
 
 function Cell() {
-    let value = "";
+    let value = 0;
     const addToken = (player) => {
         value = player;
     };
@@ -71,3 +97,13 @@ function createPlayer(inputName, tokenType) {
     const setPoints = () => points++;
     return { getName, getPoints, getToken, setPoints };
 }
+
+
+
+// run
+gameBoard.reset();
+gameControl.playRound([0, 0]);
+gameControl.playRound([1, 1]);
+gameControl.playRound([0, 1]);
+gameControl.playRound([2, 1]);
+gameControl.playRound([0, 2]);
